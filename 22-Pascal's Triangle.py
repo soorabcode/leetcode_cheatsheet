b34b2428a74edf38e45ca696f29dac10e5d45e13
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-01-07 07:44:58 +0545

    Add files via upload

---
+++ 22-Pascal's Triangle.py
@@ -0,0 +1,14 @@
+# DAY 22
+# Pascal's Triangle
+# In Pascal's triangle, each number is the sum of the two numbers directly above 
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        output = [[1]]  
+        for r in range(2, numRows + 1):
+            temp = [1] 
+            for c in range(1, r - 1):
+                temp.append(output[-1][c] + output[-1][c - 1])
+            temp.append(1)  
+            output.append(temp)
+        return output
