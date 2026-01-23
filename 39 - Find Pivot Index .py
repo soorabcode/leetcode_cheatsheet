d92d7deaf697c33747ec074fec3c570c39a1e257
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-01-23 06:39:52 +0545

    Add files via upload

---
+++ 39 - Find Pivot Index .py
@@ -0,0 +1,16 @@
+# Day 39 
+# Find Pivot Index 
+# Given an array of integers nums, calculate the pivot index of this array.
+# The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+# If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+# Return the leftmost pivot index. If no such index exists, return -1.
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum = 0
+        right_sum = sum(nums)
+        for i in range(len(nums)):
+            right_sum -= nums[i]   
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i] 
+        return -1
