5bbf2c1c162601f86e84f6fd86b964bc0f2ec604
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-01-24 10:16:05 +0545

    Add files via upload

---
+++ 40 - Median of Two Sorted Arrays .py
@@ -0,0 +1,26 @@
+# Day 40 
+# Median of Two Sorted Arrays
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+# return the median of the two sorted arrays.
+# The overall run time complexity should be O(log (m+n))
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        m, n = len(nums1), len(nums2)
+        lo, hi = 0, m
+        while lo <= hi:
+            i = (lo + hi) // 2
+            j = (m + n + 1) // 2 - i
+            L1 = float('-inf') if i == 0 else nums1[i - 1]
+            R1 = float('inf') if i == m else nums1[i]
+            L2 = float('-inf') if j == 0 else nums2[j - 1]
+            R2 = float('inf') if j == n else nums2[j]
+            if L1 <= R2 and L2 <= R1:
+                if (m + n) % 2 == 0:
+                    return (max(L1, L2) + min(R1, R2)) / 2
+                return max(L1, L2)
+            elif L1 > R2:
+                hi = i - 1
+            else:
+                lo = i + 1
