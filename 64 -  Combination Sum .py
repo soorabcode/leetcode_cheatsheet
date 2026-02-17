5c8a490c4d5010e62bfe854b6aadba2c5881d9c0
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-02-17 14:39:29 +0545

    Add files via upload

---
+++ 64 -  Combination Sum .py
@@ -0,0 +1,26 @@
+# Day 64
+#  Combination Sum
+# Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+# The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            
+            if i >= len(candidates) or total > target:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])  # stay at i (reuse allowed)
+            cur.pop()
+            
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return res
