89e22e2d51c5137b1b7e694d886cb80761843367
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-02-24 07:04:41 +0545

    Add files via upload

---
+++ 71 - Coin Change .py
@@ -0,0 +1,15 @@
+# Day 71 
+# Coin Change
+# You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+# Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+# You may assume that you have an infinite number of each kind of coin.
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[amount] if dp[amount] != float('inf') else -1
