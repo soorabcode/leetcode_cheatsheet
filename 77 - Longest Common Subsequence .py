c83cf60c180620ac1ca8028fd17286a941904985
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-03-02 06:55:35 +0545

    Add files via upload

---
+++ 77 - Longest Common Subsequence .py
@@ -0,0 +1,21 @@
+# Day 77
+# Longest Common Subsequence 
+# Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+# A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+# For example, "ace" is a subsequence of "abcde".
+# A common subsequence of two strings is a subsequence that is common to both strings.
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        
+        # DP table
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        return dp[m][n]
