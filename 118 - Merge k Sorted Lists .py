d4c6fbc555cc4519ca5e28279ef933d8456056f7
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-05-05 08:38:55 +0545

    Add files via upload

---
+++ 118 - Merge k Sorted Lists .py
@@ -0,0 +1,31 @@
+# Day 118 
+# Merge k Sorted Lists 
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+# Merge all the linked-lists into one sorted linked-list and return it.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+        
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+        
+        dummy = ListNode(0)
+        current = dummy
+        
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            
+            current.next = node
+            current = current.next
+            
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+        
+        return dummy.next
