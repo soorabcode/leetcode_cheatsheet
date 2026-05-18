b388578c343a6876f2f351189c9ff0380a4d1fcc
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-05-18 10:37:09 +0545

    Add files via upload

--- 1-twosum.py
+++ 1-twosum.py
@@ -1,9 +1,9 @@
-#Problem : Two Sum
+# Problem : Two Sum
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
 
-#Solution
+# Solution
 # Create a dictionary to store numbers we've seen and their indices
 # Iterate over the list with index and value
 # Calculate the complement needed to reach the target
